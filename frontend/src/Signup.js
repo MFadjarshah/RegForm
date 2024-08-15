89750d0da002a00c68cca4887ef05f67f89a1699
MFadjarshah <shah1815@gmail.com>
2024-08-15 15:48:53 +0800

    Completed but no data in mysql

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,12 +1,27 @@
-import React from "react";
-import Link from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 function Signup() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: [event.target.values] });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8081/signup", values)
+      .then((res) => console.log("Registered Successfully!!"))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="d-flex w-100 vh-100 bg-primary justify-content-center align-items-center">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign-Up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name">
               <strong>Name</strong>
@@ -16,6 +31,7 @@ function Signup() {
               placeholder="Enter Name"
               name="name"
               className="form-control rounded-0"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
@@ -28,6 +44,7 @@ function Signup() {
             placeholder="Enter Email"
             name="email"
             className="form-control rounded-0"
+            onChange={handleChange}
           />
           <div className="mb-3">
             <label htmlFor="password">
@@ -38,17 +55,19 @@ function Signup() {
               placeholder="Enter Password"
               name="password"
               className="form-control rounded-0"
+              onChange={handleChange}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
             Sign up
           </button>
-          <Link
-            to="/signup"
+          <p>You are agree to our terms and policies</p>
+          <a
+            href="/"
             className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
           >
             Create Account
-          </Link>
+          </a>
         </form>
       </div>
     </div>
